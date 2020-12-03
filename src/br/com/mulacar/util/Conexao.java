fb9753ad4a37a557322a032b6ace9e4451153026
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-12-03 15:57:06 -0300

    usuario postgres na conexao

--- src/br/com/mulacar/util/Conexao.java
+++ src/br/com/mulacar/util/Conexao.java
@@ -27,9 +27,9 @@ public class Conexao {
         if (conexao == null) {
             try {
                 String driver = "org.postgresql.Driver";
-                String url = "jdbc:postgresql://localhost:5432/mulacar";
+                String url = "jdbc:postgresql://192.168.0.13:5432/mulacar";
                 String usuario = "postgres";
-                String password = "root";
+                String password = "1357";
 
                 Class.forName(driver);
                 conexao = DriverManager.getConnection(url, usuario, password);
